f9730ebb655064ff623cc14773597b9cb34de638
༒ᶜʳᵃᶻʸᴮᴼˢˢ卂乃卄丨丂卄乇Ҝ༒ <93928612+Abhishek-Issac@users.noreply.github.com>
2025-01-05 16:27:21 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -1,49 +1,42 @@
 from pyrogram import filters, Client
+from pyrogram.enums import ParseMode
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 START_MSG = """**Hi {}
   
 I am a Media Editor bot ...
 
-You can edit/relace the documents,videos,gifs,audios,photos etc… Of Your Channels easily By Using Me**
+You can edit/replace the documents, videos, GIFs, audios, photos, etc., of your channels easily by using me.**
 
-`For More Info On Usage Hit ➟` /help 
+`For more info on usage, hit ➟` /help 
 
 """
-
 
 HELP_MSG = """
 Follow the steps...
 
-🌀First Send Me A Media That You Need To Edit/Replace The Other One
+🌀 First, send me a media file that you need to edit/replace the other one.
 
-🌀Send The Link Of The Media That Will Be Replaced/Edited
+🌀 Send the link of the media that will be replaced/edited.
 
-NB: Note both you & the bot must be an admin in the targert channel 
+NB: Both you and the bot must be admins in the target channel.
 
 """
-
-
-
-
-
 
 @Client.on_message(filters.command('start') & filters.private)
 async def start(client, message):
     await message.reply_text(
         text=START_MSG.format(message.from_user.mention),
         disable_web_page_preview=True,
-        reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton(text="OWNER",url = "t.me/jack_of_tg")]]),
-        reply_to_message_id=message.message_id,
-        parse_mode="combined"
-    )    
-
-
+        reply_markup=InlineKeyboardMarkup(
+            [[InlineKeyboardButton(text="OWNER", url="t.me/jack_of_tg")]]
+        ),
+        parse_mode=ParseMode.MARKDOWN
+    )
 
 @Client.on_message(filters.command('help') & filters.private)
 async def help(client, message):
     await message.reply_text(
         text=HELP_MSG,
-        disable_web_page_preview=True,
-        reply_to_message_id=message.message_id
-    )    
+        disable_web_page_preview=True
+    )
